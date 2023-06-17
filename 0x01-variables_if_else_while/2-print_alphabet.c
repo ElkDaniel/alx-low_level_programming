15d48079187f5c070aaf603a7b871d56285c9e46
ElkDaniel <kelechiegbule@gmail.com>
2023-06-16 18:54:27 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints program that prints the alphabet in lowercase
+ * , followed by a new line
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar (alp[i]);
+	}
+	putchar ('\n');
+	return (0);
+}
