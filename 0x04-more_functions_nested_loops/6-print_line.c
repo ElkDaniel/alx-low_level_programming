a77f6eebefd6f0e89caf2834dacdf101503160af
ElkDaniel <kelechiegbule@gmail.com>
2023-06-23 10:02:13 -0700

    printline

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,4 +9,20 @@ int _putchar(char c);
  * @n: parameter
  */
 
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
 
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
