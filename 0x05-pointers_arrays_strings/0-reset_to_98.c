462d5b0785f42b0d7bb71c2da95866b19954c6bc
ElkDaniel <kelechiegbule@gmail.com>
2023-06-26 11:47:49 -0700

    resetto98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,22 +10,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
-#include "main.h"
-#include <stdio.h>
-/**
- * main - check code
- * Return: 0 (always)
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-
-	_putchar("n=%d\n", n);
-	reset_to_98(&n);
-	_putchar("n=%d\n", n);
-	return (0);
-}
-
