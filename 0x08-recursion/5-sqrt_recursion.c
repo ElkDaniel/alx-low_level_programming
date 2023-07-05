ff7c65d2c1b5bf456503d4c8fe14b1003df34ff7
ElkDaniel <kelechiegbule@gmail.com>
2023-07-05 04:37:03 -0700

    sqrtrecursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
+int find_sqrt(int n, int guess);
+
 /**
  * _sqrt_recursion - Calculates the natural square root of a number
  * @n: The number to calculate the square root of
@@ -10,35 +12,27 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return (-1);
+		return -1;
 	else if (n == 0 || n == 1)
 		return n;
 	else
-		return _sqrt_helper(n, 1, n);
+		return find_sqrt(n, n / 2);
 }
 
 /**
- * _sqrt_helper - Recursive helper function to find square root
+ * find_sqrt - Recursive helper function to find square root
  * @n: The number to calculate the square root of
- * @start: The starting value for the range
- * @end: The ending value for the range
+ * @guess: The current guess for the square root
  *
  * Return: The natural square root of the number, or -1 if not found
  */
-int _sqrt_helper(int n, int start, int end)
+int find_sqrt(int n, int guess)
 {
-	if (start <= end)
-	{
-		int mid = (start + end) / 2;
-		int sqr = mid * mid;
+	if (guess == 0)
+		return -1;
 
-		if (sqr == n)
-			return mid;
-		else if (sqr < n)
-			return _sqrt_helper(n, mid + 1, end);
-		else
-			return _sqrt_helper(n, start, mid - 1);
-	}
-
-	return -1;
+	if (guess * guess == n)
+		return guess;
+	else
+		return find_sqrt(n, guess - 1);
 }
